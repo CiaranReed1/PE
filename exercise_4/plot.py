12561e9d35e9241c302e46cc72881363645e48f9
Ciaran Reed <ciaranreed@hotmail.co.uk>
2026-01-19 20:11:22 +0000

    plot with both bocked and unblocked

--- exercise_4/plot.py
+++ exercise_4/plot.py
@@ -10,14 +10,21 @@ exptected_peak = min(compute_roof,memory_bandwidth*AI)
 AI_range = np.logspace(-2,2,100)
 const_AI = roofline_data['flops']*0 + AI
 fig, ax = plt.subplots(1,1,figsize=(10,6))
+blocked = ["","-blocked"]
 optimisation_levels = ["O0","O1", "O2", "O3"]
-colors = ["red", "blue", "green", "orange"]
-for level in optimisation_levels:
-    roofline_data = pd.read_csv(f'roofline_{level}.out',delim_whitespace=True)
-    const_AI = roofline_data['flops']*0 + AI
-    ax.scatter(const_AI,roofline_data["flops"]*1000000, label=f'Measured Performance {level}', color=colors[optimisation_levels.index(level)],marker= "x")
+colors = ["red", "blue", "green", "orange", "purple", "brown", "pink", "gray"]
+i = 0
+for block in blocked:
+    for level in optimisation_levels:
+        roofline_data = pd.read_csv(f'roofline{block}_{level}.dat',delim_whitespace=True)
+        const_AI = roofline_data['flops']*0 + AI
+        ax.scatter(const_AI,roofline_data["flops"]*1000000, label=f'Measured Performance {level} {block}', color=colors[i],marker= "o" if block=="" else "x")
+        i += 1
+    
 ax.loglog(AI_range, np.minimum(compute_roof, memory_bandwidth*AI_range), label='Roofline Model', color='gray')
 ax.legend()
+ax.set_xlim(1e-1, 1e0)
+ax.set_ylim(1e8, 1e10)
 ax.set_xlabel('Arithmetic Intensity (FLOP/Byte)')
 ax.set_ylabel('Performance (FLOP/s)')
 ax.set_title('Roofline Plot (O0 Optimization)')
