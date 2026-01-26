658d53c691e1afb3f6b9594034537b9678286e21
Ciaran Reed <ciaranreed@hotmail.co.uk>
2026-01-26 11:42:45 +0000

    added avx, sse and fma implementations to my code

--- exercise_5/mycode.c
+++ exercise_5/mycode.c
@@ -17,13 +17,58 @@
 #endif  /* LIKWID_PERFMON */
 
 
-void myloop(int n, const double * restrict x, double * restrict y, double * restrict z){
+static void sca(int n, const double * restrict x, double * restrict y, double * restrict z){
 LIKWID_MARKER_START("MYLOOP");
+#pragma clang loop vectorize(disable)
+#pragma novector
 for (int i = 0; i < n; i++) {
   y[i] = x[i] * 2.0 + z[i];
   z[i] = z[i] + y[i];
 }
 LIKWID_MARKER_STOP("MYLOOP");
+}
+
+static void sse(int n, const double * restrict x, double * restrict y, double * restrict z){
+__m128d x_, y_, z_;
+LIKWID_MARKER_START("SSE");
+for (int i = 0; i < n; i+= 2) {
+    __m128d tmp;
+    x_ = _mm_loadu_pd(x + i);
+    z_ = _mm_loadu_pd(z + i);
+    tmp = _mm_mul_pd(x_, _mm_set1_pd(2.0));
+    y_ = _mm_add_pd(z_, tmp);
+    z_ = _mm_add_pd(z_, y_);
+    _mm_storeu_pd(z + i, z_);
+}
+LIKWID_MARKER_STOP("SSE");
+}
+
+static void avx(int n, const double * restrict x, double * restrict y, double * restrict z){
+ __m256d x_, y_, z_;
+  LIKWID_MARKER_START("AVX");
+  for (int i = 0; i < n; i += 4) {
+    __m256d tmp;
+    x_ = _mm256_loadu_pd(x + i);
+    z_ = _mm256_loadu_pd(z + i);
+    tmp = _mm256_mul_pd(x_, _mm256_set1_pd(2.0));
+    y_ = _mm256_add_pd(z_,tmp);
+    z_ = _mm256_add_pd(z_,y_);
+    _mm256_storeu_pd(z + i, z_);
+  }
+  LIKWID_MARKER_STOP("AVX");
+}
+
+static void fma(int n, const double * restrict x, double * restrict y, double * restrict z){
+ __m256d x_, y_, z_;
+  LIKWID_MARKER_START("FMA");
+  for (int i = 0; i < n; i += 4) {
+    x_ = _mm256_loadu_pd(x + i);
+    z_ = _mm256_loadu_pd(z + i);
+    y_ = _mm256_fmadd_pd(x_, _mm256_set1_pd(2.0), z_);
+    z_ = _mm256_add_pd(z_, y_);
+    _mm256_storeu_pd(z + i, z_);
+  }
+  LIKWID_MARKER_STOP("FMA");
 }
 
 int main(int argc, char **argv) {
@@ -47,9 +92,18 @@ int main(int argc, char **argv) {
         b[i] = i-10;
         c[i] = 0;
     }
-
-    myloop(n, a, b, c);
-
+    if (!strcmp(argv[2], "sca")) {
+        sca(n, a, b, c);
+    } 
+    else if (!strcmp(argv[2], "sse")) {
+        sse(n, a, b, c);
+    } 
+    else if (!strcmp(argv[2], "avx")) {
+        avx(n, a, b, c);
+    } 
+    else if (!strcmp(argv[2], "fma")) {
+        fma(n, a, b, c);
+    }
     LIKWID_MARKER_CLOSE;
     free(a);
     free(b);
