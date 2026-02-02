85dc0c6b522f8378221ef048e1c175b9ad5595e6
Ciaran Reed <ciaranreed@hotmail.co.uk>
2026-02-02 11:45:18 +0000

    now includes 32 stride in plot

--- exercise_7/plot.py
+++ exercise_7/plot.py
@@ -8,16 +8,18 @@ ax.set_yscale('log')
 ax.set_xlabel('Nrow (log scale)')
 ax.set_ylabel('Effective BW (log scale) (MBytes/s)')
 ax.set_title('Memory Benchmark Results')
-colors = ['r', 'g']
+colors = ['r', 'g','b']
 linestyles = "-","--"
-for j, source in enumerate(["transpose","transpose-blocked"]):
+for j, source in enumerate(["transpose","transpose-blocked","transpose-blocked-32"]):
     
     data = pd.read_csv(source+"_mem.dat", delim_whitespace=True)
     data_tens = data[data["Nrow"] % 10 == 0]
     data_non_tens = data[data["Nrow"] % 10 != 0]
 
     for i, data in enumerate([data_tens, data_non_tens]):
-        ax.plot(data["Nrow"], data["EffectiveBW"], marker='o', linestyle=linestyles[j], color=colors[i], label=f"{source} {'tens' if i==0 else '2^n'}")
+        ax.plot(data["Nrow"], data["EffectiveBW"], marker='o', linestyle=linestyles[i], color=colors[j], label=f"{source} {'tens' if i==0 else '2^n'}")
+        
 ax.legend()
 plt.show()
 fig.savefig("mem-bench-plot.png", dpi=300)
+
