fba6c5ade73570463f63559decca02e3d3f540e6
Ciaran Reed <ciaranreed@hotmail.co.uk>
2026-02-02 11:32:05 +0000

    added test for 32 stride length

--- exercise_7/plot.py
+++ exercise_7/plot.py
@@ -2,20 +2,22 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("mem-bench.out", delim_whitespace=True)
-data_tens = data[data["Nrow"] % 10 == 0]
-data_non_tens = data[data["Nrow"] % 10 != 0]
-
 fig, ax = plt.subplots(1,1,figsize=(8,6))
 ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_xlabel('Nrow (log scale)')
-ax.set_ylabel('Effective BW (log scale)')
+ax.set_ylabel('Effective BW (log scale) (MBytes/s)')
 ax.set_title('Memory Benchmark Results')
 colors = ['r', 'g']
-for i, data in enumerate([data_tens, data_non_tens]):
-    ax.plot(data["Nrow"], data["EffectiveBW"], marker='o', linestyle='-', color=colors[i], label='Multiple of 10' if i == 0 else 'Multiple of 2^x')
+linestyles = "-","--"
+for j, source in enumerate(["transpose","transpose-blocked"]):
+    
+    data = pd.read_csv(source+"_mem.dat", delim_whitespace=True)
+    data_tens = data[data["Nrow"] % 10 == 0]
+    data_non_tens = data[data["Nrow"] % 10 != 0]
 
+    for i, data in enumerate([data_tens, data_non_tens]):
+        ax.plot(data["Nrow"], data["EffectiveBW"], marker='o', linestyle=linestyles[j], color=colors[i], label=f"{source} {'tens' if i==0 else '2^n'}")
 ax.legend()
 plt.show()
 fig.savefig("mem-bench-plot.png", dpi=300)
