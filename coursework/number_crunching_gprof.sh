b8fa8a254ce433320b9ad70fd1334d778fb2c409
Ciaran Reed <ciaranreed@hotmail.co.uk>
2026-04-13 15:21:50 +0100

    updated script to include no inlining or reordering

--- coursework/number_crunching_gprof.sh
+++ coursework/number_crunching_gprof.sh
@@ -12,14 +12,18 @@ module purge
 module load gcc/12.2 likwid/5.2.0
 mkdir -p gprof_txt
 
-g++ number_crunching.cpp -O3 -pg -march=native -o number_crunching_O3
-
+g++ number_crunching.cpp -fno-inline -fno-reorder-functions -O3 -pg -march=native -o number_crunching_O3
+g++ number_crunching.cpp -fno-inline -fno-reorder-functions -O0 -pg -march=native -o number_crunching_O0
 for k in 1 2 3 4
 do
     N=$((k * 10000))
     ./number_crunching_O3 $N
     gprof number_crunching_O3 gmon.out > gprof_txt/gprof_O3_N${N}.txt
     rm -f gmon.out
+    ./number_crunching_O0 $N
+    gprof number_crunching_O0 gmon.out > gprof_txt/gprof_O0_N${N}.txt
+    rm -f gmon.out
 done
 
 rm -f number_crunching_O3
+rm -f number_crunching_O0
