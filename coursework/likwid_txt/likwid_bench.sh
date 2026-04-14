4c3ccdf03cb0e61eb3bf1a9a13e6bcf56722c183
Ciaran Reed <ciaranreed@hotmail.co.uk>
2026-04-14 14:41:36 +0100

    fixed benchmark sh

--- coursework/likwid_txt/likwid_bench.sh
+++ coursework/likwid_txt/likwid_bench.sh
@@ -1,4 +1,12 @@
-
+#!/bin/bash
+# 1 node
+#SBATCH -c 1  
+#SBATCH --mem=128G 
+#SBATCH --job-name="number-crunching_likwid"
+#SBATCH -o "number-crunching_likwid.out"
+#SBATCH -e "number-crunching_likwid.err" 
+#SBATCH -t 00:15:00 
+#SBATCH -p test 
 module purge
 module load gcc/12.2 likwid/5.2.0
 mkdir -p likwid_txt
